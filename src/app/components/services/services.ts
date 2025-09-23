4bffecea4717572447121d1d351f35ed6e85d158
34sea <domingosmartinho67@gmail.com>
2025-09-23 16:27:43 +0200

    learning two way binding

--- src/app/components/services/services.ts
+++ src/app/components/services/services.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms"
 
 @Component({
   selector: 'app-services',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './services.html',
   styleUrl: './services.css'
 })
 export class Services {
-
+  name = 'Martinho'
+  
   list_items = ['item 1', 'item 2', 'item 3', 'item 4']
+
+  getName(){
+    console.log(`Name: ${this.name}`)
+  }
 
 }
